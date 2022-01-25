da594116e8b323fd2c039616ea4a484a8f56de8a
Alex Widener <awidener3@gmail.com>
2022-01-24 20:02:35 -0500

    added validation to inquirer prompt

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const { listenerCount } = require('process');
 
 runInquirer = async () => {
     const response = await inquirer.prompt([
         {
+            name: 'role',
+            message: 'Enter employee role',
+            type: 'list',
+            choices: [
+                'Manager',
+                'Engineer',
+                'Intern'
+            ]
+        },
+        {
+            name: 'name',
+            message: 'Enter employee name',
             type: 'input',
-            message: 'sample message',
-            name: 'sample',
+            validate: (name) => { return name === '' ? 'Please enter a name' : true; }
+        },
+        {
+            name: 'id',
+            message: 'Enter employee ID',
+            type: 'input',
+            validate: (id) => { return isNaN(id) ? 'Please enter a number' : true; }
+        },
+        {
+            name: 'email',
+            message: 'Enter employee email',
+            type: 'input',
+            validate: (email) => { return (email.split('').find(char => char === '@')) ? true : 'Must be a valid email'; } // this needs to be changed
         }
     ])
 
