76afad9e9b96da4a400d1844b24ba28769582089
Alex Widener <awidener3@gmail.com>
2022-01-24 20:09:13 -0500

    added logic and prompts for when employee is manager/engineer/intern

--- index.js
+++ index.js
@@ -31,6 +31,27 @@ runInquirer = async () => {
             message: 'Enter employee email',
             type: 'input',
             validate: (email) => { return (email.split('').find(char => char === '@')) ? true : 'Must be a valid email'; } // this needs to be changed
+        },
+        {
+            name: 'officeNumber',
+            message: 'Enter the manager\'s office #',
+            type: 'input',
+            validate: (officeNumber) => { return isNaN(officeNumber) ? 'Please enter a number' : true; },
+            when: (response) => response.role === 'Manager'
+        },
+        {
+            name: 'github',
+            message: 'Enter the engineer\'s GitHub username',
+            type: 'input',
+            validate: (github) => { return github === '' ? 'Please enter a username' : true; },
+            when: (response) => response.role === 'Engineer'
+        },
+        {
+            name: 'school',
+            message: 'Enter the intern\'s school or university',
+            validate: (school) => { return school === '' ? 'Please enter a school or university name' : true; },
+            when: (response) => response.role === 'Intern'
+
         }
     ])
 
