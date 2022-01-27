fa0de011a2f53f055b40dec449b6edb8a7d9d098
Alex Widener <awidener3@gmail.com>
2022-01-26 20:19:28 -0500

    page is now created dynamically upon finishing the inquirer prompt

--- index.js
+++ index.js
@@ -2,28 +2,38 @@ const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const { managerQuestions, engineerQuestions, internQuestions, queryQuestion } = require('./questions')
+const { managerQuestions, engineerQuestions, internQuestions, queryQuestion } = require('./lib/questions')
+const fs = require('fs');
+const buildHtml = require('./src/page-template');
+
+const team = [];
 
 menu = () => {
 
     createManager = async () => {
         const response = await inquirer.prompt(managerQuestions);
         const manager = new Manager(response.name, response.id, response.email, response.officeNumber);
-        console.log(manager);
+        // console.log(manager);
+        team.push(manager);
+        console.log(team);
         queryNext();
     }
 
     createEngineer = async () => {
         const response = await inquirer.prompt(engineerQuestions);
         const engineer = new Engineer(response.name, response.id, response.email, response.github);
-        console.log(engineer);
+        // console.log(engineer);
+        team.push(engineer);
+        console.log(team);
         queryNext();
     }
 
     createIntern = async () => {
         const response = await inquirer.prompt(internQuestions);
         const intern = new Intern(response.name, response.id, response.email, response.school);
-        console.log(intern);
+        // console.log(intern);
+        team.push(intern);
+        console.log(team);
         queryNext();
     }
 
@@ -44,12 +54,16 @@ menu = () => {
                 break;
         
             default:
+                console.log('Oops! Something went wrong!');
                 break;
         }
     }
 
     sayGoodbye = () => {
         console.log('Goodbye!');
+        fs.writeFile('./dist/index.html', buildHtml(team), (err) => {
+            if (err) throw err;
+        });
     }
 
     createManager();
